fc61663a61786ca51949ef53e305629e39dfa279
abobakaraslam <164588250+abobakaraslam@users.noreply.github.com>
2025-03-23 10:53:52 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// File: App.tsx located in src folder
+import "./App.css"; //importing stylesheet
+function App() {
+  return (
+    <div>
+      <h1 className="pageHeader">Your Name</h1>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        {/*outer-container*/}
+        <div
+          style={{ width: "107px", height: "142px", border: "1px solid black" }}
+        >
+          {/*image-container*/}
+          <img
+            src="../src/assets/images/cv_920_1080.png"
+            alt="Portfolio Logo"
+            style={{ maxWidth: "100%", height: "auto" }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+export default App;
